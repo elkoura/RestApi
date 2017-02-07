abf8fd9d202fcec956ba926c53d5fd24bfa8e260
Youyou <elkoura.younes@gmail.com>
2017-02-07 23:25:46 +0100

    First commit with new pc

---
+++ src/main/java/my/home/projects/main.java
@@ -0,0 +1,12 @@
+package my.home.projects;
+
+/**
+ * Created by Youyou on 07/02/2017.
+ */
+public class main {
+
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+
+}
